b746935b69cc9c8e9673c5ce1947712ac12d89cc
kongheanict <kongheanict@gmail.com>
2025-09-14 13:26:13 +0700

    add static

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -10,6 +10,7 @@ SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="").split(",")
+
 # ------------------------------------------------
 # APPLICATIONS
 # ------------------------------------------------
